13aac821ce19626aaffb41d89143479d77e425b0
migel340 <loke34w@gmail.com>
2025-10-09 08:28:47 +0200

    chore: add google cred path in config.py

--- src/services/google_auth.py
+++ src/services/google_auth.py
@@ -1,25 +1,23 @@
 import datetime
 
-from src.config import GOOGLE_TOKEN_PATH, CALENDAR_SCOPES
+from src.config import GOOGLE_TOKEN_PATH, GOOGLE_CREDENTIALS_PATH, GOOGLE_API_SCOPES
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.errors import HttpError
-
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import Flow
 from pathlib import Path
 
 def get_google_auth():
     token_path = Path(GOOGLE_TOKEN_PATH)
+    credentials_path = Path(GOOGLE_CREDENTIALS_PATH)
     creds = None
     if token_path.exists():
-        creds = Credentials.from_authorized_user_file(token_path, CALENDAR_SCOPES)
+        creds = Credentials.from_authorized_user_file(f"{token_path}", GOOGLE_API_SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = Flow.from_client_secrets_file(
-                "credentials.json", scopes=CALENDAR_SCOPES
+                GOOGLE_CREDENTIALS_PATH, scopes=GOOGLE_API_SCOPES
             )
             flow.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
             auth_url, _ = flow.authorization_url(prompt='consent')
@@ -34,3 +32,4 @@ def get_google_auth():
 
 if __name__ == "__main__":
     google_service = get_google_auth()
+
